a8af64ef4578ed93dffaa5b18629f44df45a15fa
Ramnath R Iyer <riyer@optimix.dev>
2022-10-08 22:17:23 -0700

    Add more bits to the integrated development environment

--- src/ide.rs
+++ src/ide.rs
@@ -1,6 +1,8 @@
 //! Text-based user interface that serves as an integrated development environment.
 
-use std::io::Result;
+use crossterm::style::{Color, StyledContent, Stylize};
+use crossterm::{execute, queue, terminal};
+use std::io::{stdout, Result};
 
 /// Window that provides a text-based user interface.
 ///
@@ -18,13 +20,46 @@ impl Window {
 
     /// Run the integrated development environment and return a result when the user session ends.
     pub fn run(&mut self) -> Result<()> {
+        let mut out = stdout();
+
+        self.init()?;
+        terminal::enable_raw_mode()?;
+        queue!(out, terminal::EnterAlternateScreen)?;
+        self.print_banner()?;
+
+        Ok(())
+    }
+
+    /// Perform any initialization operations.
+    fn init(&mut self) -> Result<()> {
         self.init = true;
         Ok(())
     }
 
     /// Perform any cleanup operations such as resetting terminal state or restoring buffers.
-    pub fn cleanup(&mut self) -> Result<()> {
+    fn cleanup(&self) -> Result<()> {
+        let mut out = stdout();
+        execute!(out, terminal::LeaveAlternateScreen)?;
+        terminal::disable_raw_mode()
+    }
+
+    /// Print a banner with basic information about the application and brief help on navigation.
+    fn print_banner(&self) -> Result<()> {
+        print!(
+            "{} {} {}\r\n{} type {} to exit, {} for assistance\r\n",
+            self.prefix(),
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            self.prefix(),
+            ":quit".with(Color::Red),
+            ":help".with(Color::Red)
+        );
         Ok(())
+    }
+
+    /// Return the prefix to be used on output lines.
+    fn prefix(&self) -> StyledContent<&'static str> {
+        "∴".with(Color::Green)
     }
 }
 
